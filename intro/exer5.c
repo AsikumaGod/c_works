c41492c8b63ba0dc06dc28639357fd4b6a909853
Stephen Adams <jacobantoine506@gmail.com>
2023-12-23 10:23:21 -0500

    new updates

---
+++ intro/exer5.c
@@ -0,0 +1,8 @@
+// Write a program that can print out the numeric value of the newline character (\n)
+#include <stdio.h>
+int main()
+{
+    char newl = '\n';
+    printf("ANSI code of Newline character is %d\n", newl);
+    return 0;
+}
